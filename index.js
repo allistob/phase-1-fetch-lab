4a0102f8bad7763ebd3289da589a0f3ddde536fa
Allison Tobar <allisontobarrodriguez@gmail.com>
2023-05-12 13:53:27 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((data) => renderBooks(data))
+
+  //function(response){
+  //  return response.json()
+  //}
+
+  
+  
+  
+  
+  
   // To pass the tests, don't forget to return your fetch!
   
 }
@@ -15,3 +28,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+  
